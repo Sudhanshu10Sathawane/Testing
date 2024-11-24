a0c85f712dcedaaafc6d8706b24dae8a8ee2aae4
Sudhanshu10Sathawane <sudhanshusathwane@gmail.com>
2024-11-24 22:23:19 +0530

    graphBfs

--- SoftwareTesting/src/main/java/org/testing/App.java
+++ SoftwareTesting/src/main/java/org/testing/App.java
@@ -1,13 +1,44 @@
 package org.testing;
 
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 class UtilityMethods {
 
 
 }
 
 public class App {
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        int n = V;
+        int m = adj.size();
 
+        ArrayList<Integer> bfs = new ArrayList<>();
+
+        Queue<Integer> q = new LinkedList<>();
+        int[] visit = new int[n];
+
+        q.offer(0);
+        visit[0] = 1;
+
+        while(!q.isEmpty()){
+            Integer ele = q.poll();
+            bfs.add(ele);
+
+            for(Integer it : adj.get(ele)){
+                if(visit[it]==0){
+                    q.offer(it);
+                    visit[it] = 1;
+                }
+            }
+        }
+
+        return bfs;
+
+    }
 
     public static void main(String[] args) {
     }
--- SoftwareTesting/src/test/java/org/testing/AppTest.java
+++ SoftwareTesting/src/test/java/org/testing/AppTest.java
@@ -3,6 +3,58 @@ package org.testing;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-public class AppTest {
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AppTest {
+    private ArrayList<ArrayList<Integer>> createGraph(int V, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+        }
+        return adj;
+    }
+    @Test
+    public void test_bfsOfGraph() {
+        int V = 4;
+        int[][] edges = {{0, 1}, {0, 2}, {1, 2}, {2, 0}, {2, 3}, {3, 3}};
+        ArrayList<ArrayList<Integer>> adj = createGraph(V, edges);
+
+        ArrayList<Integer> expectedOutput = new ArrayList<>(Arrays.asList(0, 1, 2, 3));
+
+        // Instantiate the class containing bfsOfGraph
+        App graphBFS = new App();
+        assertEquals(expectedOutput, graphBFS.bfsOfGraph(V, adj));
+    }
+
+    @Test
+    public void testDisconnectedGraph() {
+        // Graph with disconnected components
+        int V = 5;
+        int[][] edges = {{0, 1}, {0, 2}, {3, 4}};
+        ArrayList<ArrayList<Integer>> adj = createGraph(V, edges);
+
+        ArrayList<Integer> expectedOutput = new ArrayList<>(Arrays.asList(0, 1, 2));
+
+        // Instantiate the class containing bfsOfGraph
+        App graphBFS = new App();
+        assertEquals(expectedOutput, graphBFS.bfsOfGraph(V, adj));
+    }
+    @Test
+    public void testSingleNodeGraph() {
+        // Single node graph
+        int V = 1;
+        int[][] edges = {};
+        ArrayList<ArrayList<Integer>> adj = createGraph(V, edges);
+
+        ArrayList<Integer> expectedOutput = new ArrayList<>(Arrays.asList(0));
+
+        // Instantiate the class containing bfsOfGraph
+        App graphBFS = new App();
+        assertEquals(expectedOutput, graphBFS.bfsOfGraph(V, adj));
+    }
 }
