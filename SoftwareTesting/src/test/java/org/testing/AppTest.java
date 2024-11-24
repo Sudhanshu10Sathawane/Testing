c65fafbf18cbe367b1449f07bf1408d811a66508
Sudhanshu10Sathawane <sudhanshusathwane@gmail.com>
2024-11-24 19:32:19 +0530

    First Commit

---
+++ SoftwareTesting/src/test/java/org/testing/AppTest.java
@@ -0,0 +1,8 @@
+package org.testing;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+public class AppTest {
+
+}
---
+++ SoftwareTestingIntegration/src/main/java/org/testing/App.java
@@ -0,0 +1,14 @@
+package org.testing;
+
+
+class UtilityMethods {
+
+
+}
+
+public class App {
+
+
+    public static void main(String[] args) {
+    }
+}
